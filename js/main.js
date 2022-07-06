73927e9db74038927f1d545aef8c9935bfb2e49d
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-07-06 09:52:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("I'm one!!"),console.log("I'm two!")})();
---
+++ modules/two.js
@@ -0,0 +1,9 @@
+" use strict";
+
+const twoModule = () => {
+  const twoTwo = "I'm two!!";
+
+  console.log(twoTwo);
+};
+
+export default twoModule;
