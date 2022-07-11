e30d765aa274e2bbb9b8b8507670c435cd10f32e
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-07-11 20:36:16 +0300

    Add files via upload

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,18 +4,41 @@ const modal = () => {
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
   const closeBtn = modal.querySelector(".popup-close");
-
-  let idInterval;
-
-  //   buttons.forEach((btn) => {
-  //     btn.addEventListener("click", () => {
-  //       modal.style.display = "block";
-  //     });
-  //   });
+  const popupContent = modal.querySelector(".popup-content");
 
   const openModal = () => {
     modal.style.display = "block";
-    clearInterval(idInterval);
+  };
+
+  const animate = ({ timing, draw, duration }, collbeck) => {
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+      // timeFraction изменяется от 0 до 1
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+
+      // вычисление текущего состояния анимации
+      let progress = timing(timeFraction);
+
+      draw(progress); // отрисовать её
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  };
+
+  const opacityMolal = () => {
+    animate({
+      duration: 1000,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        popupContent.style.opacty = progress;
+      },
+    });
   };
 
   buttons.forEach((btn) => {
@@ -23,12 +46,20 @@ const modal = () => {
       const clientWidthScreen = screen.width;
 
       if (clientWidthScreen > 768) {
-        //   openModal();
-        //   modal.style.display = "block";
-        idInterval = setInterval(openModal, 500);
+        openModal();
+        // modal.style.display = "block";
+        animate({
+          duration: 500,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            popupContent.style.opacity = progress;
+          },
+        });
       } else {
-        modal.style.display = "block";
-        clearInterval(modal);
+        openModal();
+        // modal.style.display = "block";
       }
     });
   });
