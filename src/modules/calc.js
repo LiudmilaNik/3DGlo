9c46694dded673422d9080cc767a3210c4422f5b
LiudmilaNik <102462552+LiudmilaNik@users.noreply.github.com>
2022-07-19 16:27:23 +0300

    Update calc.js

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -40,14 +40,13 @@ const calc = (price = 100) => {
       let start = performance.now();
 
       requestAnimationFrame(function animate(time) {
-        // timeFraction изменяется от 0 до 1
         let timeFraction = (time - start) / duration;
         if (timeFraction > 1) timeFraction = 1;
 
-        // вычисление текущего состояния анимации
+       
         let progress = timing(timeFraction);
 
-        draw(progress); // отрисовать её
+        draw(progress);
 
         if (timeFraction < 1) {
           requestAnimationFrame(animate);
